2c31157cee197b59f65e7170e1335e15f16ee2cb
matthew.smith <matthew.smith@aligent.com.au>
2022-05-16 21:56:38 +0930

    add Region filament scaffolding for admin area

---
+++ app/Filament/Resources/RegionResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RegionResource\Pages;
+use App\Filament\Resources\RegionResource\RelationManagers;
+use App\Models\Region;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class RegionResource extends Resource
+{
+    protected static ?string $model = Region::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('code')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('country_code')
+                    ->required()
+                    ->maxLength(2),
+                Forms\Components\RichEditor::make('description')
+                    ->required()
+                    ->maxLength(255)
+                    ->columnSpan(['default' => 1]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('country_code'),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRegions::route('/'),
+            'create' => Pages\CreateRegion::route('/create'),
+            'edit' => Pages\EditRegion::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/RegionResource/Pages/EditRegion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\RegionResource\Pages;
+
+use App\Filament\Resources\RegionResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditRegion extends EditRecord
+{
+    protected static string $resource = RegionResource::class;
+}
