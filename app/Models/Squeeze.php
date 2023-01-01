4bfb83f42833ea4d13bc44bb348976c9abcff2cd
hellboy1975 <hellboy1975@gmail.com>
2023-01-01 23:09:37 +1030

    Link Cave and Squeeze models

---
+++ app/Models/Squeeze.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Cave;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Squeeze extends Model
+{
+    use HasFactory;
+
+    public function cave(): BelongsTo
+    {
+        return $this->belongsTo(Cave::class, 'cave_id');
+    }
+}
