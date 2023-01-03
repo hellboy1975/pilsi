c3f100a8921e88c30dc3fcda9c65f6492d0d55b3
hellboy1975 <hellboy1975@gmail.com>
2023-01-03 10:48:11 +1030

    as Users to the belongsTo list for Squeezes

--- app/Models/Squeeze.php
+++ app/Models/Squeeze.php
@@ -9,11 +9,16 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Squeeze extends Model
 {
-    protected $fillable = ['name', 'cave_id', 'pilsi', 'description'];
+    protected $fillable = ['name', 'cave_id', 'pilsi', 'description', 'user_id'];
     use HasFactory;
 
     public function cave(): BelongsTo
     {
         return $this->belongsTo(Cave::class, 'cave_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
