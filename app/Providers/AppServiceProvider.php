e5a27c391a78b2ea6680b5c76f15c6f1cc6d00e9
hellboy1975 <hellboy1975@gmail.com>
2023-01-03 22:40:42 +1030

    add filament.css to theme registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Filament\Facades\Filament;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Filament::serving(function () {
+            Filament::registerTheme(
+                mix('resources/css/filament.css'),
+            );
+        });
     }
 }
