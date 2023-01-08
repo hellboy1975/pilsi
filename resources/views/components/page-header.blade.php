6b73e70372d53e20b3140dc7a114f7ae660f899e
hellboy1975 <hellboy1975@gmail.com>
2023-01-08 22:00:12 +1030

    add page-header and page-menu components

---
+++ resources/views/components/page-header.blade.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <link href="{{ asset('css/filament.css') }}" rel="stylesheet">
+</head>
