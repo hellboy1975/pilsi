026d80813f69136046b7d26ed2ca1ada39b9b4e6
hellboy1975 <hellboy1975@gmail.com>
2023-01-15 20:07:21 +1030

    add BelongsTo Region and User relationships to Trip model

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Region;
+use App\Models\User;
 
 class Trip extends Model
 {
+    protected $fillable = ['name', 'region_id', 'user_id', 'trip_leader', 'trip_leader_id', 'notes', 'start_date', 'end_date'];
     use HasFactory;
+
+    public function region(): BelongsTo
+    {
+        return $this->belongsTo(Region::class, 'region_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
