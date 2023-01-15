874e8eb953781bd3c1fdd1e1296b6b2a347b8e40
hellboy1975 <hellboy1975@gmail.com>
2023-01-15 16:59:56 +1030

    add Trips to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RegionSeeder::class,
             CaveSeeder::class,
-            SqueezeSeeder::class
+            SqueezeSeeder::class,
+            TripSeeder::class
         ]);
     }
 }
