9cf6d51fee8fc17fe543dbcd4fa2c19b3363945b
hellboy1975 <hellboy1975@gmail.com>
2023-01-15 21:38:13 +1030

    add BelongsTo relationships to Visit model

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+use App\Models\Trip;
+use App\Models\Cave;
+use App\Models\User;
 
 class Visit extends Model
 {
     use HasFactory;
+
+    public function trip(): BelongsTo
+    {
+        return $this->belongsTo(Trip::class, 'trip_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function cave(): BelongsTo
+    {
+        return $this->belongsTo(Cave::class, 'cave_id');
+    }
 }
