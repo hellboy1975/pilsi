5aca528671b33d5249fdd2283f049c7c5ea441f3
hellboy1975 <hellboy1975@gmail.com>
2023-01-15 22:03:54 +1030

    add necessary fillable Visit fields

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -12,6 +12,8 @@ use App\Models\User;
 
 class Visit extends Model
 {
+    protected $fillable = ['notes', 'start_date', 'end_date', 'duration', 'party_leader', 'party_leader_id', 'trip_id', 'cave_id', 'user_id'];
+
     use HasFactory;
 
     public function trip(): BelongsTo
