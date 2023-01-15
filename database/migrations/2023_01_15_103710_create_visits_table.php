e44ec6ab90b934534ae58804378c25a70a7f2272
hellboy1975 <hellboy1975@gmail.com>
2023-01-15 21:21:48 +1030

    create Visits migration

--- database/migrations/2023_01_15_103710_create_visits_table.php
+++ database/migrations/2023_01_15_103710_create_visits_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Trip;
+use App\Models\User;
+use App\Models\Cave;
 
 class CreateVisitsTable extends Migration
 {
@@ -15,6 +18,15 @@ class CreateVisitsTable extends Migration
     {
         Schema::create('visits', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Trip::class);
+            $table->foreignIdFor(User::class)->comment('This user will be the one who creates the visit');
+            $table->foreignIdFor(Cave::class);
+            $table->text('notes')->nullable();
+            $table->dateTime('start_date')->comment('The date/time on which the visit starts')->nullable();
+            $table->dateTime('end_date')->comment('The date/time on which the visit ends')->nullable();
+            $table->float('duration', 8, 2)->comment('The length of the trip.  Can be set by selecting a start/end time')->nullable();
+            $table->string('party_leader')->nullable()->comment('Optional name of the Party leader (if not an existing user)');
+            $table->foreignId('party_leader_id')->nullable();
             $table->timestamps();
         });
     }
