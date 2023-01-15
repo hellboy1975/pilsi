39dcdffc7870263a3d60f06aee82c34eda5d57d3
hellboy1975 <hellboy1975@gmail.com>
2023-01-15 21:24:26 +1030

    add generated Visit Filament resources

---
+++ app/Filament/Resources/VisitResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VisitResource\Pages;
+use App\Filament\Resources\VisitResource\RelationManagers;
+use App\Models\Visit;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VisitResource extends Resource
+{
+    protected static ?string $model = Visit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVisits::route('/'),
+            'create' => Pages\CreateVisit::route('/create'),
+            'edit' => Pages\EditVisit::route('/{record}/edit'),
+        ];
+    }    
+}
