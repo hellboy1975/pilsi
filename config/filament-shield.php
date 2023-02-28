f87731028307a86518972cf59fa718bd84dd73ae
hellboy1975 <hellboy1975@gmail.com>
2023-02-28 21:47:56 +1030

    move Filament Shield group to bottom in admin

--- config/filament-shield.php
+++ config/filament-shield.php
@@ -4,7 +4,7 @@ return [
     'shield_resource' => [
         'should_register_navigation' => true,
         'slug' => 'shield/roles',
-        'navigation_sort' => -1,
+        'navigation_sort' => 100,
         'navigation_badge' => true,
         'navigation_group' => true,
         'is_globally_searchable' => false,
