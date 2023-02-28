e65d8c3fe10bd38081afa5fbd21d63b73d12013b
hellboy1975 <hellboy1975@gmail.com>
2023-02-28 16:05:43 +1030

    add Attempt relationships to models

--- app/Models/Attempt.php
+++ app/Models/Attempt.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attempt extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['notes', 'date', 'success', 'visit_id', 'squeeze_id', 'user_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function squeeze(): BelongsTo
+    {
+        return $this->belongsTo(Squeeze::class, 'squeeze_id');
+    }
+
+    public function visit(): BelongsTo
+    {
+        return $this->belongsTo(Visit::class, 'visit_id');
+    }
 }
