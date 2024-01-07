32fba50dc9b40b9a30a50297f68326c581fdb11b
hellboy1975 <hellboy1975@gmail.com>
2024-01-07 20:43:42 +1030

    Remove password save event

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -10,12 +10,4 @@ class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
-    public function mutateFormDataBeforeSave(array $data): array 
-    {
-        if ((array_key_exists('new_password', $data)) || filled($data['new_password'])) {
-            $this->record->password = Hash::make($data['new_password']);
-        }
-
-        return $data;
-    }
 }
