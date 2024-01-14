23f23b96f792561022bad16bb3cfab859ec3d4e7
hellboy1975 <hellboy1975@gmail.com>
2024-01-14 21:58:40 +1030

    Add a fancier front end

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -2,5 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
+    @vite('resources/css/app.css')
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
