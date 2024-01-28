60c0d1d5aca13c5a08b0fa8edad50d0a5cd8d7b0
hellboy1975 <hellboy1975@gmail.com>
2024-01-28 20:44:50 +1030

    add Post model and resource

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'post_type',
+        'content',
+        'user_id',
+        'featured_image',
+        'published_at',
+        'status',
+    ];
+
+    /** @return BelongsTo<Post, Model> */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
