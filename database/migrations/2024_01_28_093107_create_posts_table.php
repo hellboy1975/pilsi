85f34f8a41b4f79e806bdbbd74837b9aaa6d7271
hellboy1975 <hellboy1975@gmail.com>
2024-01-29 22:07:48 +1030

    add parent_id to posts

--- database/migrations/2024_01_28_093107_create_posts_table.php
+++ database/migrations/2024_01_28_093107_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->index();
+            $table->foreignId('parent_id')->index();
             $table->string('title');
             $table->string('slug');
             $table->text('description')->nullable();
