0374a40b0b1c8db85c54bb146b10e703a8ea44f5
hellboy1975 <hellboy1975@gmail.com>
2024-02-03 16:30:35 +1030

    change Edit and View actions to iconbuttons

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -169,8 +169,8 @@ class PostResource extends Resource
                     ]),
             ])
             ->actions([
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make()->iconButton(),
+                Tables\Actions\EditAction::make()->iconButton(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
