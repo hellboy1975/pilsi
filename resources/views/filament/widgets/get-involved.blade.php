ac0d907bdaaf0fefffbc826103ab93d221bf449d
hellboy1975 <hellboy1975@gmail.com>
2024-02-03 16:49:56 +1030

    replace Gravatar widget with Get Involved!

--- resources/views/filament/widgets/get-involved.blade.php
+++ resources/views/filament/widgets/get-involved.blade.php
@@ -6,25 +6,25 @@
                 <h2
                     class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white"
                 >
-                    Avatars
+                    Get Involved!
                 </h2>
 
                 <p class="text-sm text-gray-500 dark:text-gray-400">
-                    You can setup your Avatar using the site <a href="https://gravatar.com">Gravatar</a>
+                    You can get involved in the development of this site via our <a href="https://github.com/hellboy1975/pilsi">Github.</a>  You can use this to make suggestions, and discuss those made by others.  You can even contribute code if you're super keen!
                 </p>
             </div>
 
             @csrf
-            <form action="https://gravatar.com">
+            <form action="https://github.com/hellboy1975/pilsi">
                 <x-filament::button
                     color="gray"
-                    icon="heroicon-m-link"
+                    icon="heroicon-m-code-bracket"
                     icon-alias="panels::widgets.account.logout-button"
                     labeled-from="sm"
                     tag="button"
                     type="submit"
                 >
-                    Gravatar.com
+                    Github
                 </x-filament::button>
             </form>
         </div>
