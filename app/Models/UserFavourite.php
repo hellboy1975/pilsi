3c7d4dd8da450541bdc978bbd4ca3f6cc10b1ab3
hellboy1975 <hellboy1975@gmail.com>
2024-02-11 22:21:29 +1030

    Add BelongsTo relationship to UserFavourite model

--- app/Models/UserFavourite.php
+++ app/Models/UserFavourite.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserFavourite extends Model
 {
     use HasFactory;
 
-    protected $table = 'user_favourite';
+    protected $table = 'user_favourites';
 
     protected $fillable = ['user_id', 'entity_id', 'entity_type'];
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
