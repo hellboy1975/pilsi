ea65dc2c2f015cd8f163e0bd1cefea0d1158dca4
hellboy1975 <hellboy1975@gmail.com>
2024-02-11 20:12:03 +1030

    rename user_favourites table to remain consistent

--- database/migrations/2024_02_04_043705_create_user_favourite_table.php
+++ database/migrations/2024_02_04_043705_create_user_favourite_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,9 +12,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_favourite', function (Blueprint $table) {
+        Schema::create('user_favourites', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')
+            $table->foreignIdFor(User::class)
                 ->index();
             $table->foreignId('entity_type')
                 ->references('id')
@@ -29,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_favourite');
+        Schema::dropIfExists('user_favourites');
     }
 };
