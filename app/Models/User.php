5d14ab32ff441df09d1b6695f35692cececc7b09
hellboy1975 <hellboy1975@gmail.com>
2024-02-11 22:22:25 +1030

    add several functions to User model to access favourites

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
@@ -72,5 +74,65 @@ class User extends Authenticatable implements FilamentUser
         }
 
         return true;
+    }
+
+    public function favouritesByEntity(string $entityType): HasMany
+    {
+        return $this->HasMany(UserFavourite::class)->where('entity_type', $entityType);
+    }
+
+    /**
+     * Filters the favourites by caves
+     *
+     * @return BelongsToMany
+     */
+    public function favourite_caves(): HasMany
+    {
+        return $this->favouritesByEntity(Cave::class);
+    }
+
+    /**
+     * Returns all of the favourites belonging to a User
+     */
+    public function favourites(): HasMany
+    {
+        return $this->hasMany(UserFavourite::class);
+
+    }
+
+    public function hasLiked(Model $entity): bool
+    {
+
+        return $this->favouritesByEntity($entity::class)->where('entity_id', $entity->id)->exists();
+    }
+
+    /**
+     * Toggles a user_favourites record for the current logged in user
+     *
+     * @return bool the current state of this favourite
+     */
+    public static function toggleFavourite(Model $entity): bool
+    {
+        $state = false;
+        $fav = UserFavourite::where([
+            ['user_id', Auth::id()],
+            ['entity_id', $entity->id],
+            ['entity_type', $entity::class],
+        ]);
+
+        if ($fav->exists()) {
+            // delete the record
+            $fav->delete();
+        } else {
+            $fav = new UserFavourite;
+            $fav->entity_id = $entity->id;
+            $fav->user_id = Auth::id();
+            $fav->entity_type = $entity::class;
+            $fav->save();
+
+            $state = true;
+        }
+
+        return $state;
     }
 }
