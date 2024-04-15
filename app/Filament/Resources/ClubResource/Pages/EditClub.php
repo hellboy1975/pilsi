9b2d835f957c1a6b3258b817a83d459c324fbcb3
hellboy1975 <hellboy1975@gmail.com>
2024-04-15 23:45:06 +0930

    update deprecated DeleteAction

--- app/Filament/Resources/ClubResource/Pages/EditClub.php
+++ app/Filament/Resources/ClubResource/Pages/EditClub.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\ClubResource\Pages;
 
 use App\Filament\Resources\ClubResource;
-use Filament\Pages\Actions;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditClub extends EditRecord
@@ -13,7 +13,7 @@ class EditClub extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            DeleteAction::make(),
         ];
     }
 }
