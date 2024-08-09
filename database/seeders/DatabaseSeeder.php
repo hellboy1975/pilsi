4d552cbf893b4d59aba4a9d15fa6193c3daeeb50
matthew.smith <matthew.smith@aligent.com.au>
2024-08-09 20:14:28 +0930

    add UserSeeder to add PiLSiBot

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,10 +11,11 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         $this->call([
             RegionSeeder::class,
+            UserSeeder::class,
             CaveSeeder::class,
             SqueezeSeeder::class,
             TripSeeder::class,
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'PiLSiBot',
+                'id' => 1,
+                'email' => 'bot@pilsi.xyz',
+                'bio' => 'I am a bot which adds caves automagically',
+            ]
+        ];
+
+        DB::table('users')->insert($users);
+    }
+}
