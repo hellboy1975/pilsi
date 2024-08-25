7074a3530f885450751c453f60474d5c0210a28b
matthew.smith <matthew.smith@aligent.com.au>
2024-08-25 19:23:40 +0930

    WIP add Trip creation wizard

--- app/Filament/Resources/TripResource/Pages/CreateTrip.php
+++ app/Filament/Resources/TripResource/Pages/CreateTrip.php
@@ -2,10 +2,31 @@
 
 namespace App\Filament\Resources\TripResource\Pages;
 
+use App\Filament\Forms\Components\LocalisedCountrySelect;
 use App\Filament\Resources\TripResource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Wizard\Step;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateTrip extends CreateRecord
 {
+    use CreateRecord\Concerns\HasWizard;
+
     protected static string $resource = TripResource::class;
+
+    protected function getSteps(): array
+    {
+        return [
+            Step::make('Region')
+                ->description('Which region is the trip to?')
+                ->schema([
+                    LocalisedCountrySelect::make('country')
+                        //TODO: use local
+                        ->default('AU'),
+                    Select::make('region_id')
+                        ->relationship('region', 'name')
+                        ->required(),
+                ])
+        ];
+    }
 }
